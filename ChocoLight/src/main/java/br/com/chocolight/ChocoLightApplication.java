430e21fc2c3b73829f1c513074c8aab764c26a1f
Gustavo Assalin <gustavo.assalin@hotmail.com>
2022-07-12 17:42:53 -0300

    Project created

---
+++ ChocoLight/src/main/java/br/com/chocolight/ChocoLightApplication.java
@@ -0,0 +1,13 @@
+package br.com.chocolight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoLightApplication.class, args);
+	}
+
+}
---
+++ ChocoLight/src/test/java/br/com/chocolight/ChocoLightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chocolight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
